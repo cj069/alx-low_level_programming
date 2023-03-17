c3cf3513fdc2d4a463f7f9c196b1facdffc11ca7
cj069 <ortsin@gmail.com>
2023-03-17 09:23:03 -0700

    C programming

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#includ < stdio.h >
+
+/**
+ * main - entry
+ *
+ * Return: always 0
+ */
+
+int main(void)
+
+{
+	printf("Size of a char: %i byte(s)\n", sizeof(char));
+	printf("Size of int: %i byte(s)\n", sizeof(int));
+	printf("Size of long int: %i byte(s)\n", sizeof(long int));
+	printf("Size of long long int: %i byte(s)\n", sizeof(long long int));
+	printf("Size of float: %i byte(s)\n", sizeof(float));
+	return (0);
+}

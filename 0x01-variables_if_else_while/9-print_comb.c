0e6fea3b86857885f8f24add6e1fd3096b62ff57
cj069 <ortsin@gmail.com>
2023-03-22 13:51:26 -0700

    variables if else while 8-9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+  * main - entry
+  * Description: print comb of numbers
+  * Return: always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
